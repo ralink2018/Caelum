2c344dd8413e722169795465050206effe5cc66b
ralink2018 <38867188+ralink2018@users.noreply.github.com>
2018-05-05 16:33:21 -0300

    Add files via upload

---
+++ Desafio/src/Teste.java
@@ -0,0 +1,10 @@
+
+public class Teste {
+	
+	//Para rodar e necessario colocar a variavel X como Static
+	static int x = 37;
+	public static void main(String[] args){
+		System.out.println(x);
+	}
+
+}
